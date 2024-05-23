b9af081eb3d1a16252a68d09380f993eeb5af0ce
Sofia Prata <158978435+sofiaprata@users.noreply.github.com>
2024-05-22 22:24:01 -0300

    tipo ternario add

---
+++ TIPO-VARIAVEIS/src/TipoTernario.java
@@ -0,0 +1,12 @@
+public class TipoTernario {
+    
+    public static void main(String[] args) {
+        int a,b;
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ?"verdadeiro" : "falso";
+//lê-se: Se a variavel resultado tiver a igual a b, então é verdadeira, senão, é falsa
+        System.out.println(resultado);
+    }
+}
