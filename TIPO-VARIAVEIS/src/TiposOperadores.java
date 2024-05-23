f924e01ce23c44372a2b1a2d1179ff4c101f152e
Sofia Prata <158978435+sofiaprata@users.noreply.github.com>
2024-05-22 22:02:21 -0300

    tipos de operadores java

---
+++ TIPO-VARIAVEIS/src/TiposOperadores.java
@@ -0,0 +1,42 @@
+
+public class TiposOperadores {
+
+    public static void main(String[] args) {
+
+//tipos de atribuições
+    String nome = "Sofia";
+    int idade = 22;
+    double peso = 68.5;
+    char sexo = 'M';
+    boolean doadorOrgao = false;
+
+    System.out.println(nome);
+    System.out.println(idade);
+    System.out.println(peso);
+    System.out.println(sexo);
+    System.out.println(doadorOrgao);
+
+//tipos de operadores aritmeticos
+    double soma = 10.5 + 15.7;
+    int subtração = 113 - 25;
+    int multiplicacao = 20 * 7;
+    int divisao = 15 / 3;
+    int modulo = 18 % 3;
+    double resultado = (10 * 7) + (20/4);
+      
+    System.out.println(soma);
+    System.out.println(subtração);
+    System.out.println(multiplicacao);
+    System.out.println(divisao);
+    System.out.println(modulo);
+    System.out.println(resultado);
+
+//Concatenação de strings
+
+        String nomePrimeiro = "Sofia ";
+        String nomeSegundo = "Prata";
+
+        System.out.println(nomePrimeiro + nomeSegundo);
+}
+
+}
--- TIPO-VARIAVEIS/src/TiposVariaveis.java
+++ TIPO-VARIAVEIS/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(numeroCurto2);
+        System.out.println(numeroCurto);
+        
     }
 }
